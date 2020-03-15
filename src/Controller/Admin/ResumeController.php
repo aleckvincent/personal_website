798b48bab9d7271d0f23a84c8ee9372902c3ef3c
Aleck VINCENT MINATCHY <aleck.vincent@gmail.com>
2020-03-15 17:14:48 +0100

    working on save resume item

--- src/Controller/Admin/ResumeController.php
+++ src/Controller/Admin/ResumeController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Resume;
 use App\Entity\User;
 use App\Form\ProfileType;
+use App\Form\ResumeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,9 +22,32 @@ class ResumeController extends AbstractController {
     public function list() {
 
        $repo = $this->getDoctrine()->getRepository(Resume::class);
+      
+
         return $this->render('admin/resume/list.html.twig', [
             'schools' => $repo->findBy(['type' => 'School']),
             'works' => $repo->findBy(['type' => 'Work'])
+        ]);
+    }
+
+     /**
+     * @Route("/save", name="admin_resume_save")
+     */
+    public function save(Request $request)
+    {
+        $resume = new Resume();
+        $form = $this->createForm(ResumeType::class, $resume);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($resume);
+            $em->flush();
+            $this->addFlash('notice', 'Nouvel élément ajouté au CV.');
+            return $this->redirectToRoute('admin_resume_list');
+        }
+        return $this->render('admin/resume/save.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/ResumeType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Resume;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResumeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'École / Formation' => 'School',
+                    'Professionnel' => 'Work'
+                ]
+            ])
+            ->add('title')
+            ->add('location')
+            ->add('start', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('end', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('ongoing', ChoiceType::class, [
+                'choices' => [
+                    'Non' => false,
+                    'Oui' => true
+                ],
+                'required' => true
+
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['class' => 'tinymce']
+            ])
+        ;
+    }
+
+   
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Resume::class,
+        ]);
+    }
+}
